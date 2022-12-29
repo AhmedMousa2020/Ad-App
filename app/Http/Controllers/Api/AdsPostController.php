91438e628e1af8366ec83af5cc6015831df1420c
AhmedMousa2020 <maxsteel2020@gmail.com>
2022-12-29 12:55:12 +0200

    ad-app

--- app/Http/Controllers/Api/AdsPostController.php
+++ app/Http/Controllers/Api/AdsPostController.php
@@ -15,11 +15,16 @@ class AdsPostController extends Controller
         $result=array();
         $ads = AdsPost::all();
         foreach($ads as $ad){
+            $tags = '';
+            foreach(AdsPost::find($ad->id)->tags()->pluck('name') as $tag){
+                $tags .=$tag.',';
+            }
+            $tags = rtrim($tags, ",");  
              $result[] = array('type'=> $ad->type,
              'title'=> $ad->title,
              'description'=> $ad->description,
              'category'=>AdsPost::find($ad->id)->adsCategory()->pluck('title')[0],
-             'tags'=> AdsPost::find($ad->id)->tags()->pluck('name')[0],
+             'tags'=> $tags,
              'advertiser'=>AdsPost::find($ad->id)->Advertiser()->pluck('name')[0],
              'start_date'=> $ad->start_date
         );
